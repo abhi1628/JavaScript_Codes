86880c951b2c7cc1c0895ae2be3b048b1829ffd6
Abhishek Singh <abhishek.ec@global.org.in>
2023-01-09 21:30:47 +0530

    Create array_push.js

---
+++ array_push.js
@@ -0,0 +1,11 @@
+//insert one element at last place
+const num = [3,4,5,6];
+num.push(7);
+console.log(num);
+//insert more than one element at last place
+num.push(8,9,10);
+console.log(num);
+//finding length of array using push
+const num = [3,4,5,6];
+const new_num = num.push(7,8,9);
+console.log(new_num);
