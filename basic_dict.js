3eff526e9709642d3a21d6a617e26059816c1527
Abhishek Singh <abhishek.ec@global.org.in>
2023-01-10 15:10:33 +0530

    Create basic_dict.js

---
+++ basic_dict.js
@@ -0,0 +1,7 @@
+//Creating a basic dictionary (called as object) in JavaScript
+let bio = new Object();
+bio['first_name'] = 'Tony';
+bio['last_name'] = 'Stark';
+bio['fav_lang'] = 'AI';
+console.log('Basic Dictionary created:')
+console.log(bio);
