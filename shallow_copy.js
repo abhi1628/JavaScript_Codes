d46ac62b4b07434e611664969efa8e6a018d1660
Abhishek Singh <abhishek.ec@global.org.in>
2023-01-10 14:52:33 +0530

    Create shallow_copy.js

---
+++ shallow_copy.js
@@ -0,0 +1,8 @@
+//Shallow copying in Array
+orgArr = ['pizza', 'burger', 'hotdog'];
+newArr = orgArr;
+newArr[0] = 'sandwich';
+console.log('The original array is:');
+console.log(orgArr)
+console.log('The new array is:');
+console.log(newArr)
