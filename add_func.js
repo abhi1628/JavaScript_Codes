a59897044dc80e8cc728d74cd691cc9241ba0c90
Abhishek Singh <abhishek.ec@global.org.in>
2023-01-11 21:10:02 +0530

    Create add_func.js

---
+++ add_func.js
@@ -0,0 +1,6 @@
+function addNum(a,b){
+  return (a+b);
+}
+let a = Number(prompt('Enter 1st number:'));
+let b = Number(prompt('Enter 2nd number:'));
+console.log('The sum of '+a+' and '+b+' is '+addNum(a,b));
